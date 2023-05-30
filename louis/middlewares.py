faa9d77c57a034b7a2ced692d6c4f84ee9928059
Ricky Ng-Adam <Ricky.Ng-Adam@inspection.gc.ca>
2023-05-30 00:43:42 +0000

    fix test and reuse fake_response

--- louis/middlewares.py
+++ louis/middlewares.py
@@ -10,6 +10,7 @@ from itemadapter import is_item, ItemAdapter
 
 from urllib.parse import urlparse
 
+from louis.fake_response import fake_response_from_file
 
 class LouisSpiderMiddleware:
     # Not all methods need to be defined. If a method is not defined,
@@ -59,36 +60,6 @@ class LouisSpiderMiddleware:
 
 
 
-import os
-
-from scrapy.http import HtmlResponse, Request
-
-def fake_response_from_file(file_name, url=None):
-    """
-    Create a Scrapy fake HTTP response from a HTML file
-    @param file_name: The relative filename from the responses directory,
-                      but absolute paths are also accepted.
-    @param url: The URL of the response.
-    returns: A scrapy HTTP response which can be used for unittesting.
-    """
-    if not url:
-        url = 'https://www.inspection.gc.ca'
-
-    request = Request(url=url)
-    if not file_name[0] == '/':
-        responses_dir = os.path.dirname(os.path.realpath(__file__))
-        file_path = os.path.join(responses_dir, file_name)
-    else:
-        file_path = file_name
-
-    with open(file_path, 'r') as f:
-        file_content = f.read()
-
-    response = HtmlResponse(url=url,
-        request=request,
-        body=file_content,
-        encoding='utf-8')
-    return response
 
 class LouisDownloaderMiddleware:
     # Not all methods need to be defined. If a method is not defined,
--- tests/test_goldie.py
+++ tests/test_goldie.py
@@ -1,7 +1,13 @@
 import unittest
 
-from responses import fake_response_from_file
+from louis.fake_response import fake_response_from_file
 from louis.spiders.goldie import GoldieSpider
+
+# get directory of this file
+# https://stackoverflow.com/questions/5137497/find-current-directory-and-files-directory'
+import os
+cwd = os.path.dirname(os.path.abspath(__file__))
+
 
 class TestGoldie(unittest.TestCase):
 
@@ -12,8 +18,6 @@ class TestGoldie(unittest.TestCase):
         returned_results = []
         for item in results:
             self.assertIsNotNone(item['url'])
-            self.assertIsNotNone(item['title'])
-            self.assertIsNotNone(item['subtitle'])
             returned_results.append(item)
         self.assertEqual(len(returned_results), expected_length)
         return returned_results
@@ -21,23 +25,22 @@ class TestGoldie(unittest.TestCase):
     def test_sample1(self):
         url="https://inspection.canada.ca/inspection-and-enforcement/enforcement-of-the-sfcr/eng/1546989322632/1547741756885"
         response = fake_response_from_file(
-            '1547741756885.html', 
+            f'{cwd}/responses/1547741756885.html', 
             url=url)
         results = self.spider.parse(response)
-        results = self._test_item_results(results, 3)
-        self.assertEqual(results[0]['title'], 'Enforcement of the Safe Food for Canadians Regulations')
-        self.assertEqual(results[0]['subtitle'], 'How is CFIA enforcing the SFCR?')
+        results = self._test_item_results(results, 1)
+        self.assertEqual(results[0]['title'], 'Enforcement of the Safe Food for Canadians Regulations - Canadian Food Inspection Agency')
         self.assertEqual(results[0]['url'], url)
-        self.assertTrue(results[0]['content'].startswith(
-            "CFIA's enforcement approach to the SFCR balances the need to protect Canada's food safety"))
+        self.assertTrue(results[0]['html_content'].startswith(
+            '<html><body><main class="container" property="mainContentOfPage" typeof="WebPageElement"> <h1 id="wb-cont" property="name">Enforcement of the <i>Safe Food for Canadians Regulations</i>'))
 
     def test_sample2(self):
         url="https://inspection.canada.ca/food-safety-for-industry/toolkit-for-food-businesses/understanding-the-sfcr/eng/1492029195746/1492029286734"
         response = fake_response_from_file(
-            '1492029286734.html', 
+            f'{cwd}/responses/1492029286734.html', 
             url=url)
         results = self.spider.parse(response)
-        results = self._test_item_results(results, 7)
+        results = self._test_item_results(results, 1)
 
 if __name__ == '__main__':
     unittest.main()
