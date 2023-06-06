a3fbec8a0c0ffbf6b04712f55499b6b441bf25af
Ricky Ng-Adam <Ricky.Ng-Adam@inspection.gc.ca>
2023-06-06 20:20:37 +0000

    new db tests (green)

--- louis/db.py
+++ louis/db.py
@@ -1,7 +1,11 @@
+"""Database functions for the Louis project."""
+
 import psycopg2
 import psycopg2.extras
+import psycopg2.sql as sql
 
 def connect_db():
+    """Connect to the postgresql database and return the connection."""
     connection = psycopg2.connect(database="inspection.canada.ca")
     psycopg2.extras.register_uuid()
     connection.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
@@ -38,7 +42,7 @@ def store_chunk_item(cursor, item):
         data['token_id'] = cursor.fetchone()[0]
 
         return item
-    except psycopg2.IntegrityError as integrity_error:
+    except psycopg2.IntegrityError:
         # ignore duplicates and keep processing
         return item
 
@@ -57,23 +61,28 @@ def store_crawl_item(cursor, item):
             )
         )
         return item
-    except psycopg2.IntegrityError as e:
+    except psycopg2.IntegrityError:
         # ignore duplicates and keep processing
         return item
 
 def store_embedding_item(cursor, item):
     """Process an EmbeddingItem and insert it into the database."""
     try:
+        data = {
+            'token_id': item["token_id"],
+            'embedding': item["embedding"],
+            'embedding_model': item["embedding_model"],
+        }
+        query = sql.SQL(
+                'INSERT INTO public.{embedding_model} (token_id, embedding)'
+                ' VALUES (%(token_id)s, %(embedding)s)'
+            ).format(embedding_model=sql.Identifier(data['embedding_model'])).as_string(cursor)
         cursor.execute(
-            "INSERT INTO public.embedding (chunk_id, embedding, embedding_model) VALUES (%s, %s, %s)",
-            (
-                item["chunk_id"],
-                item["embedding"],
-                item["embedding_model"],
-            )
+           query,
+            data
         )
         return item
-    except psycopg2.IntegrityError as integrity_error:
+    except psycopg2.IntegrityError:
         # ignore duplicates and keep processing
         return item
 
@@ -89,33 +98,40 @@ def link_pages(cursor, source_url, destination_url):
             "SELECT id FROM public.crawl WHERE url = %(source_url)s ORDER BY last_updated DESC LIMIT 1",
             data
         )
-        data['source_crawl_id'] = cursor.fetchone()[0]
+        data['source_crawl_id'] = cursor.fetchone()['id']
         cursor.execute(
             "SELECT id FROM public.crawl WHERE url = %(destination_url)s ORDER BY last_updated DESC LIMIT 1",
             data
         )
-        data['destination_crawl_id'] = cursor.fetchone()[0]
+        data['destination_crawl_id'] = cursor.fetchone()['id']
         cursor.execute(
-            "INSERT INTO public.link (source_crawl_id, destination_crawl_id) VALUES (%(source_crawl_id)s, %(destination_crawl_id)s)",
+            "INSERT INTO public.link (source_crawl_id, destination_crawl_id)"
+            " VALUES (%(source_crawl_id)s, %(destination_crawl_id)s)",
             data
         )
-    except psycopg2.IntegrityError as as integrity_error:
+    except psycopg2.IntegrityError:
         # ignore duplicates and keep processing
         return
 
 
 def fetch_links(cursor, url):
+    """Fetch all links from a given url."""
     data = {
         'source_url': url
     }
     cursor.execute(
-        "SELECT url FROM public.link WHERE source_crawl_id = (SELECT id FROM public.crawl WHERE url = %(url)s ORDER BY last_updated DESC LIMIT 1)",
+        "SELECT url FROM public.link"
+        " JOIN public.crawl ON public.link.destination_crawl_id = public.crawl.id"
+        " WHERE source_crawl_id = ("
+            " SELECT id FROM public.crawl WHERE url = %(source_url)s"
+            " ORDER BY last_updated DESC LIMIT 1)",
         data
     )
-    data['destination_urls'] = cursor.fetchall()
+    data['destination_urls'] = [r[0] for r in cursor.fetchall()]
     return data['destination_urls']
 
 def fetch_chunk_id_without_embedding(cursor):
+    """Fetch all chunk ids without an embedding."""
     cursor.execute(
         "SELECT chunk_id FROM public.chunk"
         " JOIN public.token ON public.chunk.id = public.token.chunk_id"
@@ -127,21 +143,30 @@ def fetch_chunk_id_without_embedding(cursor):
     return cursor.fetchall()
 
 def fetch_crawl_row(cursor, url):
+    """Fetch the most recent crawl row for a given url."""
     data = {
         'url': url
     }
     cursor.execute(
-        "SELECT id FROM public.crawl WHERE url = %(url)s ORDER BY last_updated DESC LIMIT 1",
+        "SELECT * FROM public.crawl WHERE url = %(url)s ORDER BY last_updated DESC LIMIT 1",
         data
     )
     return cursor.fetchone()
 
-def fetch_chunk_token(cursor, chunk_id):
+def fetch_chunk_token_row(cursor, chunk_id):
+    """Fetch the most recent chunk token for a given chunk id."""
     data = {
         'chunk_id': chunk_id
     }
     cursor.execute(
-        "SELECT chunk_token FROM public.chunk_token WHERE chunk_id = %(chunk_id)s ORDER BY id DESC LIMIT 1",
+        "SELECT * FROM public.chunk"
+        " JOIN public.token ON public.chunk.id = public.token.chunk_id"
+        " JOIN public.crawl ON public.chunk.crawl_id = public.crawl.id"
+        " WHERE public.chunk.id = %(chunk_id)s LIMIT 1",
         data
     )
     return cursor.fetchone()
+
+def cursor(connection):
+    """Return a cursor for the given connection."""
+    return connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,20 +1,32 @@
 import unittest
 import time
 
-from louis.items import ChunkItem, CrawlItem
-from louis.pipelines import process_chunk_item, connect_db, process_crawl_item
+import louis.items as items
+import louis.db as db
+
+import uuid
 
 class TestDB(unittest.TestCase):
+    """Test the database functions"""
     def setUp(self):
-        self.connection = connect_db()
+        self.connection = db.connect_db()
 
     def tearDown(self):
         self.connection.close()
 
-    def test_process_chunk_item(self):
-        """sample test to check if process_chunk_item works"""""
+    def test_schema(self):
+        """sample test to check if the schema is correct and idempotent"""
+        with open("schema.sql", encoding='utf-8') as schema_file:
+            schema = schema_file.read()
+
+        with db.cursor(self.connection) as cursor:
+            cursor.execute(schema)
+            self.connection.rollback()
+
+    def test_store_chunk_item(self):
+        """sample test to check if store_chunk_item works"""""
         with self.connection.cursor() as cursor:
-            process_chunk_item(cursor, ChunkItem({
+            db.store_chunk_item(cursor, items.ChunkItem({
                 "url": "https://inspection.canada.ca/splash",
                 "title": "Test Title",
                 "text_content": "Test Text Content",
@@ -23,10 +35,10 @@ class TestDB(unittest.TestCase):
             }))
             self.connection.rollback()
 
-    def test_process_crawl_item(self):
-        """sample test to check if process_crawl_item work"""
-        with self.connection.cursor() as cursor:
-            process_crawl_item(cursor, CrawlItem({
+    def test_store_crawl_item(self):
+        """sample test to check if store_crawl_item work"""
+        with db.cursor(self.connection) as cursor:
+            db.store_crawl_item(cursor, items.CrawlItem({
                 "url": "https://inspection.canada.ca/splash",
                 "title": "Test Title",
                 "lang": "fr",
@@ -35,3 +47,39 @@ class TestDB(unittest.TestCase):
                 "last_crawled": time.time()
             }))
             self.connection.rollback()
+
+    def test_store_embedding_item(self):
+        """sample test to check if store_embedding_item works"""
+        with db.cursor(self.connection) as cursor:
+            db.store_embedding_item(cursor, items.EmbeddingItem({
+                "token_id": "00000000-0000-0000-0000-000000000000",
+                "embedding": list(range(0, 1536)),
+                "embedding_model": "text-embedding-ada-002"
+            }))
+            self.connection.rollback()
+
+    def test_link_pages_and_fetch_links(self):
+        """sample test to check if link_pages works"""
+        with db.cursor(self.connection) as cursor:
+            source_url = "https://inspection.canada.ca/splash"
+            destination_url = "https://inspection.canada.ca/animal-health/terrestrial-animals/exports/pets/australia/eng/1321292836314/1321292933011"
+            db.link_pages(cursor, source_url, destination_url)
+            links = db.fetch_links(cursor, "https://inspection.canada.ca/splash")
+            self.connection.rollback()
+        self.assertEqual(links, [destination_url])
+
+    def test_fetch_crawl_row(self):
+        """sample test to check if fetch_crawl_row works"""
+        with db.cursor(self.connection) as cursor:
+            row = db.fetch_crawl_row(cursor, "https://inspection.canada.ca/splash")
+            self.connection.rollback()
+        self.assertEqual(row['url'], "https://inspection.canada.ca/splash")
+        self.assertEqual(row['id'], uuid.UUID("37ea48dc-f082-44fe-b48d-b4e6b92582ed"))
+
+    def test_fetch_chunk_row(self):
+        """sample test to check if fetch_chunk_row works"""
+        with db.cursor(self.connection) as cursor:
+            # select id from chunk join crawl ON public.chunk.crawl_id = public.crawl.id where url = 'https://inspection.canada.ca/splash'
+            row = db.fetch_chunk_token_row(cursor, "5f3f01f1-0772-43a0-94b8-8547651a3562")
+            self.connection.rollback()
+        self.assertEqual(row['url'], "https://inspection.canada.ca/splash")
