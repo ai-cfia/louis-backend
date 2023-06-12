11e74bad1fcc4d2485a334db9ca00dba3442ee56
Ricky Ng-Adam <Ricky.Ng-Adam@inspection.gc.ca>
2023-06-12 13:34:55 +0000

    add test for smartsearch

---
+++ tests/test_smartsearch.py
@@ -0,0 +1,12 @@
+import flask_unittest
+
+
+class TestSmartSearch(flask_unittest.AppTestCase):
+    def create_app(self):
+        from smartsearch import app
+        return app
+
+    def test_basic_query(self, app):
+        with app.test_client() as client:
+            rv = client.post('/search', json={'query': "What is the CFIA?"})
+            self.assertEqual(len(rv.json), 10)
