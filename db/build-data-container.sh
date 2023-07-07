c0ff4501573339b1e977d4c3f1317eec628e0d68
Ricky Ng-Adam <Ricky.Ng-Adam@inspection.gc.ca>
2023-07-07 21:01:13 +0000

    issue #9: introduce MOUNT_DATA for mounting inputs from local directory

--- db/build-data-container.sh
+++ db/build-data-container.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+DIRNAME=`dirname $0`
+
 if [ -z "$1" ]; then
     echo "usage: $0 LOUIS_SCHEMA"
     exit 1
 fi
 
-if [ ! -d "dumps/$1" ]; then
+if [ ! -d "$DIRNAME/dumps/$1" ]; then
     echo "No such schema $1, setting nocopy"
     BUILD_ENV=nocopy
 else
@@ -14,4 +16,7 @@ fi
 
 LOUIS_SCHEMA=$1
 echo "building $LOUIS_SCHEMA"
-podman build  --build-arg LOUIS_SCHEMA=$LOUIS_SCHEMA,BUILD_ENV=$BUILD_ENV -t louis-data:$LOUIS_SCHEMA .
+podman build --format docker \
+    --build-arg LOUIS_SCHEMA=$LOUIS_SCHEMA,BUILD_ENV=$BUILD_ENV \
+    -t louis-data:$LOUIS_SCHEMA \
+    .
