a94bf7e5b66e24996d03ad951d095e04b986c5a0
Ricky Ng-Adam <Ricky.Ng-Adam@inspection.gc.ca>
2023-07-07 19:28:43 +0000

    issue #9: create a self-encapsulated data + loading script container

--- db/lib.sh
+++ db/lib.sh
@@ -1,4 +1,4 @@
 PGBASE="inspectioncanadaca"
-PSQL_ADMIN="psql -v ON_ERROR_STOP=1 -U postgres --single-transaction -d $PGBASE"
-TARGET_SCHEMA=louis_v002
+PSQL_ADMIN="psql $LOUIS_DSN -v ON_ERROR_STOP=1 -U postgres --single-transaction"
+TARGET_SCHEMA=louis_v003
 TODAY=`date +%Y-%m-%d`
